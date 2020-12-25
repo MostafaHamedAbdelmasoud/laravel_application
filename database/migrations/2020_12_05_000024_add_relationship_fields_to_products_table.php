59488c7dced9fa5989c69caa214d1d9b1b95235a
mostafa hamed <mostafa.hamed1944@gmail.com>
2020-12-26 00:18:46 +0200

    add cascaded on delete in migrations in products table

--- database/migrations/2020_12_05_000024_add_relationship_fields_to_products_table.php
+++ database/migrations/2020_12_05_000024_add_relationship_fields_to_products_table.php
@@ -12,25 +12,27 @@ class AddRelationshipFieldsToProductsTable extends Migration
 
         Schema::table('products', function (Blueprint $table) {
             $table->unsignedBigInteger('trader_id')->nullable();
-            $table->foreign('trader_id', 'trader_fk_2504438')->references('id')->on('traders');
+            $table->foreign('trader_id', 'trader_fk_2504438')->references('id')->on('traders')->cascadeOnDelete();
 
             $table->unsignedBigInteger('main_product_type_id')->nullable();
-            $table->foreign('main_product_type_id')->references('id')->on('main_product_types');
+            $table->foreign('main_product_type_id')->references('id')->on('main_product_types')->cascadeOnDelete();
 
             $table->unsignedBigInteger('sub_product_type_id')->nullable();
-            $table->foreign('sub_product_type_id')->references('id')->on('sub_product_types');
+            $table->foreign('sub_product_type_id')->references('id')->on('sub_product_types')->cascadeOnDelete();
 
             $table->unsignedBigInteger('sub_product_service_type_id')->nullable();
-            $table->foreign('sub_product_service_type_id')->references('id')->on('sub_product_service_types');
+            $table->foreign('sub_product_service_type_id')->references('id')
+                ->on('sub_product_service_types')->cascadeOnDelete();
 
             $table->unsignedBigInteger('main_product_service_type_id')->nullable();
-            $table->foreign('main_product_service_type_id')->references('id')->on('main_product_service_types');
+            $table->foreign('main_product_service_type_id')->references('id')
+                ->on('main_product_service_types')->cascadeOnDelete();
 
             $table->unsignedBigInteger('city_id')->nullable();
-            $table->foreign('city_id')->references('id')->on('cities');
+            $table->foreign('city_id')->references('id')->on('cities')->cascadeOnDelete();
 
             $table->unsignedInteger('department_id')->nullable();
-            $table->foreign('department_id')->references('id')->on('departments');
+            $table->foreign('department_id')->references('id')->on('departments')->cascadeOnDelete();
         });
         Schema::enableForeignKeyConstraints();
     }
