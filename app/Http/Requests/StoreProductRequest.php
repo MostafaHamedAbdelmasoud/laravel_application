3520605d0db1f04dfde463a3f358afbdfabe127c
mostafa hamed <mostafa.hamed1944@gmail.com>
2020-12-25 18:52:13 +0200

    add nullable to store request

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -17,7 +17,7 @@ class StoreProductRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'  => [
+            'name' => [
                 'string',
                 'nullable',
             ],
@@ -26,23 +26,22 @@ class StoreProductRequest extends FormRequest
                 'required',
             ],
             'main_product_type_id' => [
-//                'required',
+                'nullable',
                 'required_without:main_product_service_type_id',
 //                'exists:main_product_types,id',
             ],
             'sub_product_type_id' => [
-//                'required',
+                'nullable',
                 'required_with:main_product_type_id',
                 'exists:sub_product_types,id',
             ],
             'main_product_service_type_id' => [
-//                'required',
+                'nullable',
                 'required_without:main_product_type_id',
 //                'exists:main_product_service_types,id',
             ],
             'sub_product_service_type_id' => [
-//                'required',
-            'nullable',
+                'nullable',
                 'required_with:main_product_service_type_id',
                 'exists:sub_product_service_types,id',
             ],
