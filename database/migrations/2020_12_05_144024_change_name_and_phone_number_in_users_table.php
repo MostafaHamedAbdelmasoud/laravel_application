f5c85ac9f5978aed1dc3f5ae18fb96e589617733
mostafa hamed <mostafa.hamed1944@gmail.com>
2021-01-09 20:06:45 +0200

    add uniqueness for email and name

---
+++ database/migrations/2020_12_05_144024_change_name_and_phone_number_in_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeNameAndPhoneNumberInUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('name')->unique()->change();
+            $table->string('phone_number')->unique()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+//        Schema::dropIfExists('users');
+    }
+}
